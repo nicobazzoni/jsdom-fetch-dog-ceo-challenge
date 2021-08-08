7abec1ca8071a7178db4b75b0f15d7611d0bf57d
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-08 17:17:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-console.log('%c HI', 'color: firebrick')
+let breeds = []
+
+document.addEventListener('DOMContentLoaded', function () {
+  loadDogImages()
+  loadBreedChoices()
+})
+
+function loadDogImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(dogPic) {
+  let container = document.querySelector('#dog-image-container')
+  let newImage = document.createElement('img')
+  newImage.src = dogPic
+  container.appendChild(newImage)
+}
+
+function loadBreedChoices() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+    .then(res => res.json())
+    .then(results => {
+
+      breeds = Object.keys(results.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
+}
+
+function updateBreedList(breeds) {
+  let ul = document.querySelector('#dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(element) {
+  let child = element.lastElementChild;
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
+
+function selectBreedsStartingWith(letter) {
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+  breedDropdown.addEventListener('change', function (event) {
+    selectBreedsStartingWith(event.target.value);
+  });
+}
+
+function addBreed(breed) {
+  let ul = document.querySelector('#dog-breeds');
+  let li = document.createElement('li');
+  li.innerText = breed;
+  li.style.cursor = 'pointer';
+  ul.appendChild(li);
+  li.addEventListener('click', updateColor);
+}
+
+function updateColor(event) {
+  event.target.style.color = 'palevioletred';
+}
